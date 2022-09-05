95f1976f81cc91d224e1f50e3a5a365d54513bc0
Carlos Teofilo <carlosneto2810@hotmail.com>
2022-09-04 21:18:25 -0400

    -

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios.apps.UsuariosConfig',
+    'porteiros.apps.PorteirosConfig',
+    'visitantes.apps.VisitantesConfig',
+]
+
+INSTALLED_APPS += [
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'controle_visitantes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -119,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from visitantes.views import (
+    informacoes_visitantes, 
+    registrar_visitante, 
+    finalizar_visita
+)
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('informacoes_visitantes/<int:id>/', informacoes_visitantes, name='informacoes_visitantes'),
+    path('registrar_visitante/', registrar_visitante, name='registrar_visitante'),
+    path('informacoes_visitantes/<int:id>/finalizar_visita/', finalizar_visita, name='finalizar_visita'),
 ]
