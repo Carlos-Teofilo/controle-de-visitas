a9f0143b5474528180d84244adee3012c3da61ba
Carlos Teofilo <carlosneto2810@hotmail.com>
2022-09-05 17:17:44 -0400

    Ordenando por ordem decrescente

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 from visitantes.models import Visitante
 
 
 def index(request):
 
-    todos_visitantes = Visitante.objects.all()
+    todos_visitantes = Visitante.objects.order_by('-horario_chegada')
+
+
+    mes_atual = timezone.now().month
 
     context = {
         'nome_pagina': 'Início da dashboard',
@@ -13,5 +17,8 @@ def index(request):
         'visitantes_aguardando': Visitante.objects.filter(status='AGUARDANDO').count(),
         'visitantes_em_visita': Visitante.objects.filter(status__startswith='EM_').count(),
         'visitantes_finalizados': Visitante.objects.filter(status__startswith='FIN').count(),
+        'visitantes_mensal': Visitante.objects.filter(
+            horario_chegada__month=mes_atual
+            ).count()
     }
     return render(request, 'index.html', context)
